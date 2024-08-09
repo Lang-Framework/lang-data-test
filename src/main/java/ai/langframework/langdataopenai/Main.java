6200c6463fc5682bd979f4fea7b571b1a096216d
blue-langboot <zulele@langboot.ai>
2024-08-09 22:58:15 +0500

    fifth test

--- src/main/java/ai/langframework/langdataopenai/Main.java
+++ src/main/java/ai/langframework/langdataopenai/Main.java
@@ -2,6 +2,6 @@ package ai.langframework.langdataopenai;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Main class of OpenAI Connector");
+        System.out.println("Main class of the Connector");
     }
 }
